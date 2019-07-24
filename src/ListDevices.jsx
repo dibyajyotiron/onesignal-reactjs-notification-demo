93784938e2c8eb9762b69167fca6e081a0e7519a
Dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-07-24 18:55:59 +0530

    removed unnecessary imports

--- src/ListDevices.jsx
+++ src/ListDevices.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import SendIcon from '@material-ui/icons/Send';
 import axios from "axios";
 
 class ListDevice extends Component {
