699766bd7c7f6bf014a17121fb977587183eca6d
Dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-07-24 18:54:33 +0530

    notification using reactjs and onesignal

---
+++ src/ListDevices.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import SendIcon from '@material-ui/icons/Send';
+import axios from "axios";
+
+class ListDevice extends Component {
+    state = {
+        totalCount: 0,
+        players: []
+    }
+
+    classes = makeStyles(theme => ({
+        button: {
+            margin: theme.spacing(1),
+        },
+        rightIcon: {
+            marginLeft: theme.spacing(1),
+        },
+        root: {
+            flexGrow: 1,
+        },
+        card: {
+            maxWidth: 345,
+        },
+    }));
+
+    async componentDidMount() {
+        const data = (await axios.get(`https://onesignal.com/api/v1/players?app_id=${process.env.REACT_APP_APP_ID}`, { headers: this.headers })).data;
+        this.setState({ totalCount: data.total_count, players: data.players });
+    };
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": `Basic ${process.env.REACT_APP_REST_API_KEY}`
+    };
+
+    render() {
+
+        return (<>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant="h4" component="h2">Total subscribers: {this.state.totalCount}</Typography>
+                    <Typography variant="body2" color="textPrimary" component="p">
+                        {this.state.players.map(el => <div style={{ border: "2px solid gray" }} key={el.id}>
+                            <p>ip:{el.ip}</p>
+                            <p>deviceType:{el.device_type}</p>
+                            <p>os:{el.device_os}</p>
+                            <br />
+                        </div>)}
+                    </Typography>
+
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+        </>)
+    }
+}
+
+export default ListDevice;
